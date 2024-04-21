5a4df7ba7b501b2a83af58249b2b1bd71aedddd5
Vamsi Krishna <87894521+vamsikrishnavusa654@users.noreply.github.com>
2024-04-21 18:21:29 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Monitoring_human_health_issues_due_to_pesticides_in_agriculture.settings')
